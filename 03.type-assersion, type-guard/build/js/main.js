659f78b005944840be607a92127d54139457e799
mmingoo <aldrn0323@naver.com>
2025-08-23 13:00:45 +0900

    assertion, type guard 스터디

--- 03.type-assersion, type-guard/build/js/main.js
+++ 03.type-assersion, type-guard/build/js/main.js
@@ -1,5 +1,4 @@
 "use strict";
-let boolean;
 let falsBoolean = false;
 let number;
 let integer = 6;
@@ -28,3 +27,61 @@ let tuple2;
 tuple2 = ['a', 1];
 tuple2.push(2);
 console.log(tuple2);
+//unknown
+let unknown = false;
+// 불가함 : 컾마일러 입장에서 불리안 안에 불리안만 넣을 수 있음. 따라서 해당 타입을 as boolean 을 지정해주면 끝 
+// let string1: boolean = unknown;
+let string2 = unknown;
+//Object
+let obg = {};
+let arr = [];
+// 원래 Null 도 object 로 인식은 되지만 원래는 Null object 객체가 아님 
+// config 파일에서 strict 를 false 로 한다면 애러 발생 안함
+// let nul : object = null; 
+let date = new Date();
+const obj1 = {
+    id: 1,
+    title: "title1"
+};
+const user2 = {
+    id: 1,
+    title: "wtirng"
+};
+//union 타입
+let union12;
+union12 = 'h1';
+union12 = 1;
+//함수 선언 
+let func1;
+func1 = function (arg1, arg2) {
+    return arg1 + arg2;
+};
+//반환타입 없는 메서드
+let func2;
+func2 = function () {
+    console.log("hi!");
+};
+// //Null, Undefinde
+// let number11123 : number = undefined;
+// let string090 : string = null;
+// let obj: {a:10, b:false} = undefined;
+// let array : any[] = null;
+// let nul1: null = undefined;
+// let void1 : void = null;
+// let void2: void = undefinedl;
+//void
+function greeting() {
+    console.log('h1');
+}
+const h1 = greeting();
+console.log(h1);
+//never
+function throwError() {
+    throw new Error('error');
+}
+function keepPricessing() {
+    while (true) {
+    }
+}
+const never = [];
+never.push(1);
--- 03.type-assersion, type-guard/src/main.ts
+++ 03.type-assersion, type-guard/src/main.ts
@@ -1,125 +1,37 @@
-let falsBoolean : boolean = false;
+// 만약에 index.html에서 body 가 없으면 타입스크립트에서 null 이라고 생각할 수 있음
+//Null 반환 가능성
 
-let number : number;
-let integer : number = 6;
-let float : number = 3.14;
+// 첫번째 방법
+const bodyElement = document.querySelector('body') as HTMLBodyElement;
+bodyElement.innerText = "hello";
 
-let string : string
-let firstName  = 'Doe';
-let lasstName  = 'my';
+//두번째
+//!를 붙여서 널이 아니라고 알려주는 것 
+const bodyElement1 = document.querySelector('body');
+bodyElement!.innerText = "hello";
 
-//array
-//한가지 타입만 가지는 배열
-let names1: string[] = ['join','kim'];
-let names2: Array<string> = ['john', 'kim'];
-
-//여러 타입을 가지는 배열 (유니온 타입 사용)
-let array1: (string | number) [] = ["John", 1,2];
-let array2: Array<string | number> = ["John", 1,2];
-
-//여러 타입
-let anyArray: Array<any>  = ['1',1,2,false] 
-
-//읽기 전용 배열 생성
-let stringArray : readonly string[] = ['a','n'];
-let numberArray : ReadonlyArray<number> = [1,2,3];
-
-
-
-//튜플
-let tuple1 : [string,number] = ['a',1];
-tuple1 = ['b',1];
-
-let users: [number , string][]
-users = [[21,'name'], [19,'kim']]
-
-
-let tuple2 :[string, number]
-tuple2 = ['a',1]
-tuple2.push(2)
-console.log(tuple2)
-
-
-//unknown
-let unknown: unknown = false;
-// 불가함 : 컾마일러 입장에서 불리안 안에 불리안만 넣을 수 있음. 따라서 해당 타입을 as boolean 을 지정해주면 끝 
-// let string1: boolean = unknown;
-let string2: boolean = unknown as boolean;
-
-
-//Object
-let obg : object = {};
-let arr : object = [];
-
-// 원래 Null 도 object 로 인식은 되지만 원래는 Null object 객체가 아님 
-// config 파일에서 strict 를 false 로 한다면 애러 발생 안함
-// let nul : object = null; 
-
-let date : object = new Date();
-
-const obj1 : {id: number , title : string} = {
-    id : 1,
-    title : "title1"
-}
-
-const user2 :  {id : number , title : string} = {
-    id : 1,
-    title : "wtirng"
-} 
-
-//union 타입
-let union12 : (string | number)
-union12 = 'h1';
-union12 = 1;
-
-
-//함수 선언 
-let func1 : (arg1 :number, arg2 :number ) => number;
-
-func1 = function(arg1, arg2){
-    return arg1 + arg2;
+// 세번째
+//타입 가이드를 사용하기도 함 
+const bodyElement2 = document.querySelector('body');
+if(bodyElement){
+    bodyElement2.innerText = "hello";
 }
 
 
-//반환타입 없는 메서드
-let func2 : () => void;
-func2 = function(){
-    console.log("hi!")
+//타입 단언은 조심히 써야함
+function func1(arg: String | null){
+    return (arg as string).toLowerCase();   
 }
 
-// //Null, Undefinde
-// let number11123 : number = undefined;
-// let string090 : string = null;
-// let obj: {a:10, b:false} = undefined;
-// let array : any[] = null;
+// hello 를 넣어줄 땐 에러가 발생하진 않음
+// 근데 Null 을 넣어줄 땐 에러가 발생
+// 그래서 타입 단언을 사용할 경우 null 이 들어가면 에러가 발생하기 때문에 조심해야 함
+func1('hello');
+func1(null) 
 
-// let nul1: null = undefined;
-// let void1 : void = null;
-// let void2: void = undefinedl;
-
-
-
-//void
-
-function greeting() : void{
-    console.log('h1')
-}
-
-const h1 : void = greeting();
-console.log(h1);
-
-//never
-function throwError(): never{
-    throw new Error('error')
-}
-
-
-function keepPricessing() : never{
-    while(true) {
-
+// 타입 가드를 사용해서 문제를 해결할 수 있음
+function func2(arg: String | null){
+    if(arg){
+        return arg .toLowerCase();   
     }
-    
 }
-
-const never: number[] = []
-never.push(1)
